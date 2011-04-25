f414c32ec7d72e2a46270ee8f5b7614326e77097
Mailo Svetel <development@rooland.cz>
2011-04-25 19:51:02 +0200

    Today - activities' name are shortened with full name in title of a element.
    Closes #19.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def link_to_with_full_title string, url
+    link_to truncate(
+              string,
+              :length => 65,
+              :omission => "..."
+            ),
+            url,
+            :title => string
+  end
 end
