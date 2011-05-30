05dccd64ec7bfa0f16061d42cf4e28db395060b4
Mailo Svetel <development@rooland.cz>
2011-05-30 22:45:09 +0200

    #21 - tags - requires user, work on own data

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,17 +1,37 @@
 class TagsController < ApplicationController
+  before_filter :require_user
+
   def index
-    @tags = ActiveRecord::Base.connection.execute('SELECT * FROM tags')
+    @tags = current_user.owned_tags
   end
 
   def show
-    @tag = ActiveRecord::Base.connection.select_one('SELECT * FROM tags WHERE id = '+params[:tag_id])
-    @datetimes = Datetime.tagged_with [@tag['name']], :any => true
+    @tag = ActiveRecord::Base.connection.select_one(
+        ' SELECT tg.id, tg.name
+          FROM tags tg
+            INNER JOIN taggings tgg ON tgg.tag_id = tg.id AND tagger_id = '+ current_user.id.to_s() +'
+          WHERE tg.id = '+params[:tag_id]
+    )
+
+    raise ActiveRecord::RecordNotFound if @tag.nil?
+
+    @datetimes = current_user.datetimes.tagged_with [@tag['name']], :any => true
   end
 
   def destroy
-    ActiveRecord::Base.connection.execute('DELETE FROM tags WHERE id = '+params[:tag_id])
+    ActiveRecord::Base.connection.execute(
+        ' DELETE FROM tags
+          WHERE id = (
+            SELECT tg.id
+            FROM tags tg
+              INNER JOIN taggings tgg ON tgg.tag_id = tg.id AND tagger_id = '+ current_user.id.to_s() +'
+            WHERE tg.id = '+params[:tag_id]+'
+            LIMIT 1
+          )
+        '
+    )
     
-    redirect_to(tags_path, :notice => 'Datetime was successfully created.')
+    redirect_to(tags_path, :notice => 'Tag was successfully destroyed.')
   end
 
 end
