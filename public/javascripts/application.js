7d4fc705a23fab20d6954b505b6cfebca2e4b0d7
Mailo Svetel <development@rooland.cz>
2011-10-08 21:18:20 +0200

    Today - current spent time is continuously updated by JS

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,39 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+function initSpentTime(currentSpentTime)
+{
+    currentSpentTimeSplitted = (currentSpentTime).split(':');
+
+    currentDateTime = new Date(); // TimedTrack(current_time);
+    currentDateTime.setHours(parseInt(currentSpentTimeSplitted[0]),parseInt(currentSpentTimeSplitted[1]));
+
+    spentTimeUpdateInterval = setInterval(
+        'currentDateTime.setSeconds(currentDateTime.getSeconds()+1)',
+        1000
+    );
+}
+
+function updateCurrentSpentTimeElement()
+{
+    hours = fulfillNumber(currentDateTime.getHours());
+    minutes = fulfillNumber(currentDateTime.getMinutes());
+
+    currentSpentTimeString = hours+':'+minutes;
+    $('activity-spent-time').update(currentSpentTimeString);
+}
+
+function fulfillNumber(number, decimalPoints)
+{
+    if(arguments.length == 1) decimalPoints = 2;
+
+    decimalPointsDifference = decimalPoints-String(number).length;
+
+    fulfillment = '';
+    if(decimalPointsDifference>0)
+    {
+        for(i=0;i<decimalPointsDifference;i++)
+        {
+            fulfillment += '0';
+        }
+    }
+
+    return fulfillment+String(number);
+}
