85e16723f9d69ea918236c4e02f7d9b62c194aeb
Mailo Svetel <development@rooland.cz>
2011-10-19 23:32:11 +0200

    User's datetimes are sorted now - ASC

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ActiveRecord::Base
   acts_as_tagger
   attr_accessible :username, :email, :locales_id, :timezone, :password, :password_confirmation, :icon_set
   
-  has_many  :datetimes
+  has_many  :datetimes, :order => 'datetimes.start ASC'
   belongs_to  :locale
 end
