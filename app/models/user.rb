42ee9182ef904d01bd3341e421d909f0bd7c01c8
Mailo Svetel <development@rooland.cz>
2012-01-05 15:37:44 +0100

    registration fixed a bit
    - corrected translations
    - locale saving
    - title
    - first iconset checked by default

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ActiveRecord::Base
   acts_as_authentic
   acts_as_tagger
-  attr_accessible :username, :email, :locales_id, :timezone, :password, :password_confirmation, :icon_set
+  attr_accessible :username, :email, :locale_id, :timezone, :password, :password_confirmation, :icon_set
   
   has_many  :datetimes, :order => 'datetimes.start ASC'
   belongs_to  :locale
+  accepts_nested_attributes_for :locale
 end
